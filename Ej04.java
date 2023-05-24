aa04b65e74bba90577658fa5ad248c12335f71d5
Aldana T <aldanatrombetta@gmail.com>
2023-05-24 20:33:20 -0300

    Ej04.java

---
+++ Ej04.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author aldan
+ */
+public class Ej04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre = "Aldana";
+        int edad = 31;
+        
+        System.out.println("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+    
+}
